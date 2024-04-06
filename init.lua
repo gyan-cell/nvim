18f97c6c5e75049ba59d4048812946074d68e501
gyan-cell <jha07944@gmail.com>
2024-04-06 09:58:31 +0530

    Improvements in lsp

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-print("Welcome Supreme Leader Gyan")
 require("core.mappings")
 require("core.plugins")
 require("core.options")
--- lua/core/pluginsourceconfig/nvimtree.lua
+++ lua/core/pluginsourceconfig/nvimtree.lua
@@ -4,63 +4,63 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+    vim.g.loaded_netrwPlugin = 1
 
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
+    -- optionally enable 24-bit colour
+    vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
+    -- empty setup using defaults
+    require("nvim-tree").setup()
 
--- OR setup with some options
-require("nvim-tree").setup({
-   update_focused_file = {
-    enable = true,
-    update_cwd = true,
-  },
-  renderer = {
-    root_folder_modifier = ":t",
- -- These icons are visible when you install web-devicons
-    icons = {
-      glyphs = {
-        default = "",
-        symlink = "",
-        folder = {
-          arrow_open = "",
-          arrow_closed = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
-        },
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "U",
-          deleted = "",
-          ignored = "◌",
+    -- OR setup with some options
+    require("nvim-tree").setup({
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+      },
+      renderer = {
+        root_folder_modifier = ":t",
+        -- These icons are visible when you install web-devicons
+        icons = {
+          glyphs = {
+            default = "",
+            symlink = "",
+            folder = {
+              arrow_open = "",
+              arrow_closed = "",
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+              symlink_open = "",
+            },
+            git = {
+              unstaged = "",
+              staged = "S",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "U",
+              deleted = "",
+              ignored = "◌",
+            },
+          },
         },
       },
-    },
-  },
-  diagnostics = {
-    enable = true,
-    show_on_dirs = true,
-    icons = {
-      hint = " ",
-      info = " ",
-      warning = " ",
-      error = " ",
-    },
-  },
-  view = {
-    width = 30,
-    side = "left",
-  },
-})
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+          hint = " ",
+          info = " ",
+          warning = " ",
+          error = " ",
+        },
+      },
+      view = {
+        width = 30,
+        side = "left",
+      },
+    })
   end,
 }
