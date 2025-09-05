502fd6d42509ceaaf8280af7f9a2cfca3f8eed41
gyan-cell <hellogyanranjan@gmail.com>
2025-09-05 19:58:07 +0530

    Android 17 Suppport

--- lua/core/pluginsourceconfig/lspZero.lua
+++ lua/core/pluginsourceconfig/lspZero.lua
@@ -5,7 +5,6 @@ return {
     lazy = true,
     config = false,
     init = function()
-      -- Disable automatic setup, we are doing it manually
       vim.g.lsp_zero_extend_cmp = 0
       vim.g.lsp_zero_extend_lspconfig = 0
     end,
@@ -15,7 +14,6 @@ return {
     lazy = false,
     config = true,
   },
-  -- LSP
   {
     'neovim/nvim-lspconfig',
     cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
@@ -25,15 +23,10 @@ return {
       { 'williamboman/mason-lspconfig.nvim' },
     },
     config = function()
-      -- This is where all the LSP shenanigans will live
       local lsp_zero = require('lsp-zero')
       lsp_zero.extend_lspconfig()
 
-      --- if you want to know more about lsp-zero and mason.nvim
-      --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
       lsp_zero.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
         lsp_zero.default_keymaps({ buffer = bufnr })
       end)
 
@@ -47,7 +40,8 @@ return {
       require('mason-lspconfig').setup({
         ensure_installed = {
           'ts_ls', 'rust_analyzer', 'clangd', 'pyright', 'lua_ls',
-          'jdtls', 'cssls', 'tailwindcss', 'prismals', 'html', "gopls"
+          'cssls', 'tailwindcss', 'prismals', 'html', "gopls"
+          -- Don't include jdtls in ensure_installed since we're installing manually
         },
         handlers = {
           lsp_zero.default_setup,
@@ -55,36 +49,49 @@ return {
             local lua_opts = lsp_zero.nvim_lua_ls()
             require('lspconfig').lua_ls.setup(lua_opts)
           end,
-          jdtls = function()
-            local home = os.getenv("HOME")
-            local jdtls_path = home .. "/.local/share/nvim/mason/packages/jdtls"
-            local launcher = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
-            local config = jdtls_path .. "/config_linux"
-            local workspace = home .. "/.cache/jdtls/workspace"
-
-            require('lspconfig').jdtls.setup {
-              cmd = {
-                "java",
-                "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-                "-Dosgi.bundles.defaultStartLevel=4",
-                "-Declipse.product=org.eclipse.jdt.ls.core.product",
-                "-Dlog.protocol=true",
-                "-Dlog.level=ALL",
-                "-Xms1g",
-                "--add-modules=ALL-SYSTEM",
-                "--add-opens", "java.base/java.util=ALL-UNNAMED",
-                "--add-opens", "java.base/java.lang=ALL-UNNAMED",
-                "-jar", launcher,
-                "-configuration", config,
-                "-data", workspace,
-              },
-              root_dir = require('lspconfig').util.root_pattern(
-                ".git", "mvnw", "gradlew", "pom.xml", "build.gradle"
-              ),
-              filetypes = { "java" },
-            }
-          end
         }
+      })
+
+      -- Manual jdtls setup for Java 17
+      local home = os.getenv("HOME")
+      local jdtls_path = home .. "/.local/share/nvim/mason/packages/jdtls"
+
+      -- Check if we have the compatible version
+      local launcher = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+      if vim.fn.filereadable(launcher) == 0 then
+        vim.notify("jdtls for Java 17 not found. Run the installation script.", vim.log.levels.WARN)
+        return
+      end
+
+      local config_dir = jdtls_path .. "/config_linux"
+      local workspace_dir = home .. "/.cache/jdtls/workspace"
+
+      -- Create workspace directory if it doesn't exist
+      os.execute("mkdir -p " .. workspace_dir)
+
+      require('lspconfig').jdtls.setup({
+        cmd = {
+          "java",
+          "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+          "-Dosgi.bundles.defaultStartLevel=4",
+          "-Declipse.product=org.eclipse.jdt.ls.core.product",
+          "-Dlog.protocol=true",
+          "-Dlog.level=ALL",
+          "-Xms1g",
+          "--add-modules=ALL-SYSTEM",
+          "--add-opens", "java.base/java.util=ALL-UNNAMED",
+          "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+          "-jar", launcher,
+          "-configuration", config_dir,
+          "-data", workspace_dir,
+        },
+        root_dir = require('lspconfig.util').root_pattern(
+          ".git", "mvnw", "gradlew", "pom.xml", "build.gradle"
+        ),
+        filetypes = { "java" },
+        on_attach = function(client, bufnr)
+          lsp_zero.default_keymaps({ buffer = bufnr })
+        end,
       })
     end
   }
